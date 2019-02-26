609f8a8c53a027ff54a0ecf8f0099cd9120e89c5
Wayne Malcolm Hafely II <wmalcolmh@gmail.com>
2019-02-25 22:42:19 -0500

    Added some comments/documentation

--- ImageToMap.py
+++ ImageToMap.py
@@ -4,26 +4,30 @@ import math
 from itertools import product
 import argparse
 
-# reference = [109.850, 100.000, 35.585]
-# reference = [95.044, 100.000, 108.755]
-reference = [94.811, 100.000, 107.304]
+reference = [94.811, 100.000, 107.304] # This is used to make our LAB <--> RGB conversions
 
 def pack_rgb_image(im: Image.Image) -> np.array:
+    """Takes a standard three dimensional array (x, y, rgb) and converts it to a two dimensional array (x, y)
+        where the values stored are single 32-bit rgb integers
+    """
     arr = np.array(im)
     x_size, y_size, _ = arr.shape
+
     out = np.zeros((x_size, y_size, 4), dtype=np.uint8)
-    out[:,:,0:3] = arr
-    out = out.view(dtype=np.uint32)
-    out = out.reshape(arr.shape[:-1])
+    out[:,:,0:3] = arr  # copy the original array into the out array (will produce [r, g, b, 0] arrays)
+    out = out.view(dtype=np.uint32)  # View the [r, g, b, 0] array as a 32-big integer
+    out = out.reshape(arr.shape[:-1])  # Remove the third dimension
     return out
 
 def convert_image_to_lab(arr:np.array):
+    """Convert an image in the RGB color space to the LAB color space"""
     converted = arr.astype(dtype=np.float64)
     for x, y in product(range(arr.shape[0]),range( arr.shape[1])):
         converted[x,y] = convert_rgb_to_lab(arr[x,y])
     return converted
 
 def convert_rgb_to_lab(color:np.array):
+    """Convert an [R, G, B] array to a [L, a, b] array"""
 
     # first we have to convert to XYZ space
     xyz = color/255.0
@@ -64,7 +68,7 @@ def convert_rgb_to_lab(color:np.array):
 
 
 def convert_lab_to_rgb(color:np.array):
-    # first we have to convert to XYZ
+    # First we have to convert to XYZ
 
     xyz = color.copy()/1.0
     xyz[1] = (color[0] + 16.0) / 116.0
@@ -84,6 +88,7 @@ def convert_lab_to_rgb(color:np.array):
 
     xyz = xyz/100.0
 
+    # Now convert to RGB
     rgb = xyz.copy()
 
     rgb[0] = xyz[0] * 3.2406 + xyz[1] * -1.5372 + xyz[2] * -0.4986
@@ -101,9 +106,16 @@ def convert_lab_to_rgb(color:np.array):
 
 
 def map_image(image: Image.Image, template: Image.Image) -> Image.Image:
+    """Create a color/template mapped image with the given images
+
+    The form of the final image will come from the template
+    The colors of the final image will come from the image
+    """
+    # Resize the image to be the same as the template
     size = template.size
     image = image.resize(size)
 
+    # Make sure our images are in the RGB color space
     if not image.mode == 'RGB':
         image = image.convert('RGB')
 
@@ -111,9 +123,11 @@ def map_image(image: Image.Image, template: Image.Image) -> Image.Image:
         template = template.convert('RGB')
 
 
+    # Grab pixel arrays of each image (with respective conversions)
     image_pix = convert_image_to_lab(np.array(image))
     template_pix = pack_rgb_image(template)
 
+    # Here is where we build the color mapping
     colors = dict()
     for y, x in product(range(size[0]), range(size[1])):
         t_pix = template_pix[x, y]
@@ -122,15 +136,17 @@ def map_image(image: Image.Image, template: Image.Image) -> Image.Image:
         if t_pix not in colors:
             colors[t_pix] = []
 
-        colors_list = colors[t_pix]
-        colors_list.append(i_pix)
+        colors[t_pix].append(i_pix)
 
+    # Mix the collected colors (A simple average) and convert back to the RGB space
     for key, value in colors.items():
         colors[key] = convert_lab_to_rgb(np.mean(value, axis=0, dtype=np.float64))
 
+    # Create an image to apply the final mapping
     mapped_image: Image.Image = Image.new('RGB', size)
     mapped_pixels = mapped_image.load()
 
+    # Apply the color map
     for y, x in product(range(size[0]), range(size[1])):
         t_pix = tuple(colors[template_pix[x, y]])
         mapped_pixels[y, x] = t_pix
